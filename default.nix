4bfd168ea660551f08ff7c6a029e75a97d93df9f
Carl Hedgren <carl.hedgren@creuna.no>
2018-09-11 20:33:53 +0200

    chore(nix): update default.nix

--- default.nix
+++ default.nix
@@ -1,2 +1,16 @@
-{ pkgs ? import <nixpkgs> {}, compiler ? "ghc822" }:
-pkgs.haskell.packages.${compiler}.callPackage ./barnehage-service.nix { }
+{ mkDerivation, aeson, base, servant-server, servant-swagger
+, sqlite-simple, stdenv, swagger2, text, transformers, wai-cors
+, wai-extra, warp
+}:
+mkDerivation {
+  pname = "BarnehageService";
+  version = "0.1.0.0";
+  src = ./.;
+  isLibrary = false;
+  isExecutable = true;
+  executableHaskellDepends = [
+    aeson base servant-server servant-swagger sqlite-simple swagger2
+    text transformers wai-cors wai-extra warp
+  ];
+  license = stdenv.lib.licenses.bsd3;
+}
